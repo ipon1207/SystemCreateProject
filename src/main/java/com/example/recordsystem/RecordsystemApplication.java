858aef95ddbb9298ad3ecf4fed38346c78de6719
ipon <iphoneipon1207@gmail.com>
2024-11-21 19:20:33 +0900

    Normalize line endings

---
+++ src/main/java/com/example/recordsystem/RecordsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.recordsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordsystemApplication.class, args);
+	}
+
+}
