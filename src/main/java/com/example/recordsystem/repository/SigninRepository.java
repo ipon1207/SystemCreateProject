e8c347e6b96aa835fc6fa16ed7e59acf7f5ad5db
ipon <iphoneipon1207@gmail.com>
2024-11-23 15:07:37 +0900

    DBに対する検索実行機能の追加

--- src/main/java/com/example/recordsystem/repository/SigninRepository.java
+++ src/main/java/com/example/recordsystem/repository/SigninRepository.java
@@ -1,5 +1,25 @@
 package com.example.recordsystem.repository;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class SigninRepository {
     
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Map<String, Object> findById(String id) {
+
+        // SELECT文
+        String query = "SELECT" + " * " + "FROM user" + " " + "WHERE id=?";
+
+        // 検索実行
+        Map<String, Object> user = jdbcTemplate.queryForMap(query);
+
+        return user;
+    }
 }
