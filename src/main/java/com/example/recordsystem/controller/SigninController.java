62db51e50f1ff25186d1d02747d723d8f6c96ce2
ipon <iphoneipon1207@gmail.com>
2024-11-23 22:44:30 +0900

    SigninController.javaのリファクタリング

--- src/main/java/com/example/recordsystem/controller/SigninController.java
+++ src/main/java/com/example/recordsystem/controller/SigninController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.recordsystem.repository.User;
+import com.example.recordsystem.model.User;
 import com.example.recordsystem.service.SigninService;
 
 @Controller
