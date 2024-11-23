a66e205bcbacf9bd207f0ead787a305301183fd3
ipon <iphoneipon1207@gmail.com>
2024-11-23 14:37:11 +0900

    Revert "サインイン機能のテスト"
    This reverts commit 9813df861aaa7d75bda6c8564a31425a40f9a687.

--- src/main/java/com/example/recordsystem/controller/SigninController.java
+++ src/main/java/com/example/recordsystem/controller/SigninController.java
@@ -1,21 +1,5 @@
 package com.example.recordsystem.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
 public class SigninController {
     
-    // signin.htmlからの入力を受け取る
-    @RequestMapping("/user/signin")
-    public String postSignin(@RequestParam("user_name") String user_Name, Model model) {
-
-        // 画面から受けとった文字列をmodelに登録
-        model.addAttribute("userName", user_Name);
-
-        // main.htmlに画面遷移
-        return "/user/main";
-    }
 }
