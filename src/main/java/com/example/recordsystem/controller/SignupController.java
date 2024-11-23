eb5ca56534ff528ba69b488b2b67b13e0b2f454e
ipon <iphoneipon1207@gmail.com>
2024-11-23 23:20:49 +0900

    signin.htmlからsignup.htmlに画面遷移するように変更

--- src/main/java/com/example/recordsystem/controller/SignupController.java
+++ src/main/java/com/example/recordsystem/controller/SignupController.java
@@ -1,5 +1,19 @@
 package com.example.recordsystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.recordsystem.service.SignupService;
+
+@Controller
 public class SignupController {
     
+    @Autowired
+    SignupService service;
+
+    @GetMapping("/user/signin")
+    public String getSignup() {
+        return "user/signup";
+    }
 }
