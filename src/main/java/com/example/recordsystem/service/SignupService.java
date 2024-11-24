d1a3bbddea455e1366ceb2ab6e83ab5bd054aa8f
ipon <iphoneipon1207@gmail.com>
2024-11-25 08:33:30 +0900

    SignupService.javaの追加

--- src/main/java/com/example/recordsystem/service/SignupService.java
+++ src/main/java/com/example/recordsystem/service/SignupService.java
@@ -1,8 +1,30 @@
 package com.example.recordsystem.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.recordsystem.model.User;
+import com.example.recordsystem.repository.SignupRepository;
+
+import jakarta.transaction.Transactional;
+
 @Service
+@Transactional
 public class SignupService {
     
+    @Autowired
+    SignupRepository repository;
+
+    // usersテーブルから全件を取得
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    // usersテーブルに入力値を登録
+    public void insert(User user) {
+        repository.save(user);
+    }
+
 }
